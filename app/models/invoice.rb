2cebd0505075196fec7d61487e9fa80f6d817b09
Eduardo Serrano <eduardoserrano8591@gmail.com>
2021-10-04 12:53:26 -0500

    creates validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
 class Invoice < ApplicationRecord
   belongs_to :reservation
+  validates :reservation, :paid, presence: true
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ApplicationRecord
+  STATUS = %i[apply reply confirmation cancelled invoiced]
   belongs_to :user
   belongs_to :ship
+  has_one :invoice
+  validates :user, :ship, :price, :people_in_reservation, :reservation_start, :reservation_end, :negotiation_status, presence: true
+  validates :price, :number_of_people, numericality: {greater_than: 0}
+  validates :negotiation_status, inclussion: STATUS
+  validates :number_of_people, numericality: {less_than_or_equal_to: :ship.max_people}
 end
