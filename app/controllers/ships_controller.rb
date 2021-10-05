8b20695b1ea5f55e52bcf6f77fa34231b35c3dc3
cesar madera <camgcamg11@gmail.com>
2021-10-05 14:19:37 -0500

    done the creation of a ship

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -4,6 +4,21 @@ class ShipsController < ApplicationController
     @ships = Ship.all
   end
 
+  def new
+    @ship = Ship.new
+  end
+
+  def create
+    @ship = Ship.new(ship_params)
+    @user = current_user
+    @ship.user = @user
+    if @ship.save
+      redirect_to ship_path(@ship)
+    else
+      render :new
+    end
+  end
+
   def ship_params
     params.require(:ship).permit(:max_people,:price, :ship_type, :latitude, :longitude, :name, :description, images: [])
   end
