96b74f2507bf8df0c523e53ee2a550705c295013
cesar madera <camgcamg11@gmail.com>
2021-10-06 15:27:05 -0500

    query by country

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -7,5 +7,5 @@ class Ship < ApplicationRecord
   validates :ship_type, inclusion: TYPES
   validates :price, :max_people, numericality: { greater_than: 0 }
   include Filterable
-
+  reverse_geocoded_by :latitude, :longitude, address: :loc
 end
